8736412c10c6f2e8dab7097445b4227c2c3646f4
Brecht Gosselé <brecht.gossele@gmail.com>
2024-02-13 20:45:48 +0100

    WIP

--- bingo/src/App.tsx
+++ bingo/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
+import DropZone from './components/DropZone';
 
 const handleClick = () => {
   console.log("Boem paukenslag!");
@@ -12,6 +13,7 @@ function App() {
       <header className="App-header">
 
         <h1>Bingo generator</h1>
+        {/* <DropZone /> */}
         <div>
           <Button variant="contained" color="primary" onClick={handleClick}>
             Boem paukenslag!
