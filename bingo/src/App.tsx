67013e567621548786c99934ec102145f20b6b66
Brecht Gosselé <brecht.gossele@gmail.com>
2024-02-13 20:27:55 +0100

    enable auto formatting

--- bingo/src/App.tsx
+++ bingo/src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+
+const handleClick = () => {
+  console.log("Boem paukenslag!");
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-    
-        <Button variant="contained" color="primary">
-          Boem paukenslag!
-        </Button>
-        
+
+        <h1>Bingo generator</h1>
+        <div>
+          <Button variant="contained" color="primary" onClick={handleClick}>
+            Boem paukenslag!
+          </Button>
+        </div>
       </header>
     </div>
   );
