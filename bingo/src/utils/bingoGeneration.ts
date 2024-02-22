4e60966ef499f61a2b57fa02daf355045869a731
Brecht Gosselé <brecht.gossele@gmail.com>
2024-02-22 16:33:26 +0100

    bingo algoritme

---
+++ bingo/src/utils/bingoGeneration.ts
@@ -0,0 +1,19 @@
+export function generateNumberSet(
+  aantalZinnenPerSet: number,
+  aantalBingoSets: number,
+  aantalBeschikbareZinnen: number,
+): Set<number>[] {
+  const bingoSets: Set<number>[] = [];
+
+  // Generate numbers based on the input
+  for (let i = 0; i <= aantalBingoSets; i += 1) {
+    const numberSet: Set<number> = new Set<number>();
+    do {
+      numberSet.add(Math.floor(Math.random() * aantalBeschikbareZinnen));
+    } while (numberSet.size < aantalZinnenPerSet);
+
+    bingoSets.push(numberSet);
+  }
+
+  return bingoSets;
+}
